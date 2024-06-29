791ce81ca304fd967ac113cdadc6881748caacd0
MUGANE ABEL <muganeabel@gmail.com>
2024-06-29 22:41:45 +0300

    vertical wins completed

--- python_tic_tac_toe.py
+++ python_tic_tac_toe.py
@@ -1,32 +1,50 @@
 game = [[2,0,1],
-        [1,0,0],
-        [1,2,0]]
+        [2,0,0],
+        [2 ,2,0]]
 
-def win(current_game):
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#horizontal winner
+
+'''def win(current_game):
     for row in game:
         print (row)
         if row.count(row[0]) == len(row) and row[0] != 0:
             print ("winner!!!")
-win(game)
- 
-
- 
+win(game)'''
 
 
+#Vertical winners
+'''for col in range(len(game)):
+    check= []
+    for row in game:
+        check.append(row[col])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if check.count(check[0]) == len(check) and check[0] != 0:
+        print ("Winner!")'''
 
 
 #Error handling example
