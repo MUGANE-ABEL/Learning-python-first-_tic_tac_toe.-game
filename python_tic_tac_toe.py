3f1c9144ff2cea1eecb79f7378fdf7a6bf7803c3
MUGANE ABEL <muganeabel@gmail.com>
2024-07-02 22:30:31 +0300

    Fun version

--- python_tic_tac_toe.py
+++ python_tic_tac_toe.py
@@ -125,34 +125,7 @@ while play:
 
 
 
-#game= game_board(game, just_display=True)
-#game= game_board(game_board, player=1, row=3, column=1)
 
-
-
-
-
-
-
-
-
-
-
-'''game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],]
-
-def game_board(player=0, row=0, column=0, just_display=False):
-    print ("   0  1  2")
-    if not just_display:
-        game[row][column]= player
-    for count, row in enumerate(game):
-        print(count, row )
-
-game_board(just_display=True)
-game_board(1,2,1)
-#game[0][1] = 1
-#game_board()'''
 
 
 ### Exercise test to see wherther you can,
